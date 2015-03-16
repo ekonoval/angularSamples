2af1b357ad5d7fff637e45b2b5b66c5186010a9c
Yehor Konoval <ekonoval@gmail.com>
2015-03-16 18:34:24 +0200

    custom

--- 1820EN_09_Code/03_custom_pag/custom-pag.js
+++ 1820EN_09_Code/03_custom_pag/custom-pag.js
@@ -1,17 +1,64 @@
 angular.module('custom-pag', [])
-.directive('custom_pagination', function () {
+.directive('customPagination', function () {
+
     return {
         restrict: 'E',
         replace: true,
-        scope: {},
+        scope: {
+            inP: '@',
+            //numPages: '=numPages'
+            numPagesInner: '=numPages',
+            currentPage: '=',
+            onSelectPage: '&'
+        },
         template:
-            '<div class="pagination"><ul>' +
+            '<div class="pagination">' +
+            '<ul>' +
                 '<li ng-class="{disabled: noPrevious()}"><a ng-click="selectPrevious()">Previous</a></li>' +
                 '<li ng-repeat="page in pages" ng-class="{active: isActive(page)}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
                 '<li ng-class="{disabled: noNext()}"><a ng-click="selectNext()">Next</a></li>' +
-            '</ul></div>',
+            '</ul>' +
+            '<div style="margin: 5px;">interpolating "{{inP}}"</div>' +
+            '</div>',
         link: function (scope) {
+            scope.$watch('numPagesInner', function (value) {
+                scope.pages = [];
+                for (var i = 1; i <= value; i++) {
+                    scope.pages.push(i);
+                }
 
+                if (scope.currentPage > value) {
+                    scope.currentPage = value;
+                }
+            });
+
+            scope.cbHandler = function(page){
+                alert("xxxxx");
+            };
+
+            scope.selectPage = function (page) {
+                scope.currentPage = page;
+
+                scope.onSelectPage({page: page});
+            };
+
+            scope.isActive = function (page) {
+                return scope.currentPage == page;
+            };
+
+            scope.selectPrevious = function () {
+                if (scope.currentPage > 1) {
+                    scope.currentPage--;
+                    scope.selectPage(scope.currentPage);
+                }
+            };
+
+            scope.selectNext = function () {
+                if (scope.currentPage < scope.pages.length) {
+                    scope.currentPage++;
+                    scope.selectPage(scope.currentPage);
+                }
+            };
         }
     };
 });
